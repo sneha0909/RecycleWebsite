5a6752f0ad563c7b83cb5edf8b9dced22e3174df
Sneha Shukla <snehadefence090998@gmail.com>
2022-06-27 22:39:11 +0700

    Publish to heroku

---
+++ client/src/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './app/layout/style.css';
+import App from './app/layout/App';
+import reportWebVitals from './reportWebVitals';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export const history = createBrowserHistory();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+
+
+  <Router history={history}>
+    <React.StrictMode>
+      
+        <Provider store={store}>
+        <App />
+        </Provider>   
+      
+    </React.StrictMode>
+  </Router>
+
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
